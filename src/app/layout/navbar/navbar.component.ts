5f3366546855acacb7472d0bc274de8647e07e54
Rose McColl <rosemccoll@hotmail.com>
2020-08-05 17:18:02 +1200

    clean up nav component

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,70 +1,34 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { LoginService, UserInfoDto } from '@uoa/auth';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '@uoa/auth';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit, OnDestroy {
-  authenticated: boolean;
-  userInfo: UserInfoDto;
+export class NavbarComponent implements OnInit {
   routes = [
     { path: '/my-projects', label: 'My Projects' },
     { path: '/my-services', label: 'My Services' },
     { path: '/help', label: 'Help' },
   ]
-  // TODO: handle subscriptions better
-  loggedInSub: Subscription;
-  userInfoSub: Subscription;
 
   constructor(public loginService: LoginService, private router: Router) { }
   
   async ngOnInit() {
-    await this.loginService.isAuthenticated();
-
-    this.loggedInSub = this.loginService.loggedIn$.subscribe(
-      (loggedIn: boolean) => { this.authenticated = loggedIn },
-      (err) => { console.log('loggedIn$ Observable error: ' + err) }
-    );
+    const authenticated = await this.loginService.isAuthenticated();
         
-    if (this.authenticated) {
+    if (authenticated) {
       await this.loginService.getUserInfo();
-
-      this.userInfoSub = this.loginService.userInfo$.subscribe(
-        (userInfo: UserInfoDto) => { this.userInfo = userInfo },
-        (err) => { console.log('userInfo$ Observable error: ' + err) }
-      )
     }
-    
-    // TODO: userInfo.groups is in unexpected format..
-    // var groups: string = <string><unknown>this.userInfo.groups;
-    // console.log(groups.substring(1, groups.length-1).split(", "));
   }
 
   async login() {
-    const success = await this.loginService.doLogin(this.router.url);
-    if (success) {
-      this.authenticated = await this.loginService.isAuthenticated();
-      this.userInfo = await this.loginService.getUserInfo();
-
-      this.loginService.userInfo$
-    }
+    await this.loginService.doLogin(this.router.url);
   }
 
   logout() {
     this.loginService.logout();
-  }
-
-  ngOnDestroy(): void {
-    if (this.loggedInSub != null) {
-      this.loggedInSub.unsubscribe()
-    }
-
-    if (this.userInfoSub != null) {
-      this.userInfoSub.unsubscribe()
-    }
   }
 }
