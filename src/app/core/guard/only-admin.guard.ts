985e1626779f77cb21ca1d8b9651ee00b4fd484a
Rose McColl <rosemccoll@hotmail.com>
2020-08-14 09:33:20 +1200

    add only-admin route guard

---
+++ src/app/core/guard/only-admin.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OnlyAdminGuard implements CanActivate {
+  constructor(
+    private router: Router
+  ) {}
+
+  // currently disallows any access to admin-only pages.
+  // TO DO - implement check for user role
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    this.router.navigate(['/error/403']);
+    return false;
+  }
+  
+}
