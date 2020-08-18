21b047e7072b7a6df99d88a0da882bd98ba7deaf
Rose McColl <rosemccoll@hotmail.com>
2020-08-18 17:36:08 +1200

    update nectar-details tests

--- src/app/modules/cer-services/nectar/nectar-details/nectar-details.component.spec.ts
+++ src/app/modules/cer-services/nectar/nectar-details/nectar-details.component.spec.ts
@@ -1,16 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ApolloTestingController, ApolloTestingModule } from 'apollo-angular/testing';
 import { NectarDetailsComponent } from './nectar-details.component';
 
 describe('NectarDetailsComponent', () => {
   let component: NectarDetailsComponent;
   let fixture: ComponentFixture<NectarDetailsComponent>;
+  let controller: ApolloTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NectarDetailsComponent ]
+      declarations: [ NectarDetailsComponent ],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        ApolloTestingModule
+      ]
     })
     .compileComponents();
+
+    controller = TestBed.get(ApolloTestingController);
   }));
 
   beforeEach(() => {
@@ -21,5 +31,9 @@ describe('NectarDetailsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  afterEach(() => {
+    controller.verify();
   });
 });
