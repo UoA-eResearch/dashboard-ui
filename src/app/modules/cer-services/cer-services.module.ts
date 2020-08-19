a863ff2d29c7b7abbd4903423a83e8550e6db57a
Rose McColl <rosemccoll@hotmail.com>
2020-08-19 14:24:30 +1200

    add new components and routes to cer-services, some minor changes

--- src/app/modules/cer-services/cer-services.module.ts
+++ src/app/modules/cer-services/cer-services.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
 import { CerServiceRoutingModule } from './cer-services.routing.module'; 
 import { CerServicesComponent } from './cer-services.component';
-import { DropboxListComponent } from './dropbox/dropbox-list.component';
+import { DropboxListComponent } from './dropbox/dropbox-list/dropbox-list.component';
 import { DropboxDetailsComponent } from './dropbox/dropbox-details/dropbox-details.component';
 import { NectarListComponent } from './nectar/nectar-list/nectar-list.component';
 import { NectarDetailsComponent } from './nectar/nectar-details/nectar-details.component';
+import { ResearchStorageListComponent } from './research-storage/research-storage-list/research-storage-list.component';
+import { ResearchStorageDetailsComponent } from './research-storage/research-storage-details/research-storage-details.component';
+import { VisualisationDetailsComponent } from './visualisation/visualisation-details/visualisation-details.component';
+import { VisualisationListComponent } from './visualisation/visualisation-list/visualisation-list.component';
+import { ResearchVmListComponent } from './research-vm/research-vm-list/research-vm-list.component';
+import { ResearchVmDetailsComponent } from './research-vm/research-vm-details/research-vm-details.component';
 
 
 @NgModule({
@@ -15,7 +21,13 @@ import { NectarDetailsComponent } from './nectar/nectar-details/nectar-details.c
     DropboxListComponent,
     DropboxDetailsComponent,
     NectarListComponent,
-    NectarDetailsComponent
+    NectarDetailsComponent,
+    ResearchStorageListComponent,
+    ResearchStorageDetailsComponent,
+    VisualisationDetailsComponent,
+    VisualisationListComponent,
+    ResearchVmListComponent,
+    ResearchVmDetailsComponent
   ],
   imports: [
     CommonModule,
--- src/app/modules/cer-services/cer-services.routing.module.ts
+++ src/app/modules/cer-services/cer-services.routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CerServicesComponent } from './cer-services.component';
-import { DropboxListComponent } from './dropbox/dropbox-list.component';
+import { DropboxListComponent } from './dropbox/dropbox-list/dropbox-list.component';
 import { DropboxDetailsComponent } from './dropbox/dropbox-details/dropbox-details.component';
 import { OnlyServiceMembersGuard } from '@app/guard/only-service-members.guard';
 import { OnlyAdminGuard } from '@app/guard/only-admin.guard';
 import { NectarListComponent } from './nectar/nectar-list/nectar-list.component';
 import { NectarDetailsComponent } from './nectar/nectar-details/nectar-details.component';
+import { ResearchStorageListComponent } from './research-storage/research-storage-list/research-storage-list.component';
+import { ResearchStorageDetailsComponent } from './research-storage/research-storage-details/research-storage-details.component';
+import { ResearchVmListComponent } from './research-vm/research-vm-list/research-vm-list.component';
+import { ResearchVmDetailsComponent } from './research-vm/research-vm-details/research-vm-details.component';
+import { VisualisationListComponent } from './visualisation/visualisation-list/visualisation-list.component';
+import { VisualisationDetailsComponent } from './visualisation/visualisation-details/visualisation-details.component';
 
 
 const routes: Routes = [
@@ -33,6 +39,36 @@ const routes: Routes = [
         path: 'nectar/:id',
         canActivate: [OnlyServiceMembersGuard],
         component: NectarDetailsComponent
+      },
+      {
+        path: 'research-storage',
+        canActivate: [OnlyAdminGuard],
+        component: ResearchStorageListComponent
+      },
+      {
+        path: 'research-storage/:id',
+        canActivate: [OnlyServiceMembersGuard],
+        component: ResearchStorageDetailsComponent
+      },
+      {
+        path: 'research-vm',
+        canActivate: [OnlyAdminGuard],
+        component: ResearchVmListComponent
+      },
+      {
+        path: 'research-vm/:id',
+        canActivate: [OnlyServiceMembersGuard],
+        component: ResearchVmDetailsComponent
+      },
+      {
+        path: 'visualisation',
+        canActivate: [OnlyAdminGuard],
+        component: VisualisationListComponent
+      },
+      {
+        path: 'visualisation/:id',
+        canActivate: [OnlyServiceMembersGuard],
+        component: VisualisationDetailsComponent
       }
     ]
   }
