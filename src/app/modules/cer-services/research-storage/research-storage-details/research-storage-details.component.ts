2dd39f741c56daa770d3604d956b11dc812e064a
Rose McColl <rosemccoll@hotmail.com>
2020-08-19 14:22:27 +1200

    add research-storage list and details components

---
+++ src/app/modules/cer-services/research-storage/research-storage-details/research-storage-details.component.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserInfoDto, LoginService } from '@uoa/auth';
+import { Subject, Subscription } from 'rxjs';
+import { Apollo } from 'apollo-angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import gql from 'graphql-tag';
+
+
+const GET_RESEARCH_STORAGE = gql`
+query ResearchStorage($id: Int!) {
+  researchstorage(id: $id) {
+    id
+    name
+    allocated_gb
+    date
+    deleted
+    division
+    free_gb
+    num_files
+    percentage_used
+    used_gb
+    projects {
+      code
+      first_day
+      last_day
+      project {
+        id
+        title
+      }
+    }
+  }
+}`;
+
+
+@Component({
+  selector: 'app-research-storage-details',
+  templateUrl: './research-storage-details.component.html',
+  styleUrls: ['./research-storage-details.component.scss']
+})
+export class ResearchStorageDetailsComponent implements OnInit, OnDestroy {
+  userInfo: UserInfoDto;
+  id;
+  researchStorage;
+  loading$ = new Subject<boolean>();
+  error;
+
+  private querySubscription: Subscription;
+  private paramsSubscription: Subscription;
+
+  constructor(
+    private loginService: LoginService,
+    private apollo: Apollo,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.paramsSubscription = this.route.params.subscribe(
+      params => {
+        this.id = parseInt(params['id']);
+      }
+    );
+  }
+
+  async ngOnInit() {
+    this.userInfo = await this.loginService.getUserInfo();
+    this.loading$.next(true);
+    if (this.userInfo && this.id) {
+      this.querySubscription = this.apollo
+      .watchQuery<any>({
+        query: GET_RESEARCH_STORAGE,
+        variables: {
+          id: this.id
+        },
+      })
+      .valueChanges
+      .subscribe(
+        ({ data, loading }) => {
+          console.log(data);
+          this.researchStorage = data.researchstorage;
+          this.loading$.next(loading);
+        },
+        error => {
+          this.loading$.next(false);          
+          if (error.message === 'GraphQL error: 404: NOT FOUND') {
+            this.router.navigate(['/notfound']);
+          }
+          else {
+            console.debug(JSON.stringify(error));
+            this.error = error;
+          }          
+        }
+      );
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.querySubscription) {
+      this.querySubscription.unsubscribe();
+    }
+
+    if (this.paramsSubscription) {
+      this.paramsSubscription.unsubscribe();
+    }
+  }
+}
---
+++ src/app/modules/cer-services/research-storage/research-storage-list/research-storage-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-research-storage-list',
+  templateUrl: './research-storage-list.component.html',
+  styleUrls: ['./research-storage-list.component.scss']
+})
+export class ResearchStorageListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
