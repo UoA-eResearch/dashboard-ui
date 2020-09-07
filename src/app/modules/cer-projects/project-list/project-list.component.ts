3e5280789393accc65de8391e7a231e5df02c574
Rose McColl <rosemccoll@hotmail.com>
2020-09-07 17:26:24 +1200

    add data and styles to project list component

--- src/app/modules/cer-projects/project-list/project-list.component.ts
+++ src/app/modules/cer-projects/project-list/project-list.component.ts
@@ -1,18 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Subject, Subscription } from 'rxjs';
+
+
+const GET_ALL_PROJECTS = gql`
+query Projects {
+  projects {
+    id
+    title
+    start_date
+    end_date
+  }
+}`;
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss']
 })
-export class ProjectListComponent implements OnInit {
+export class ProjectListComponent {
 
-  // TO DO: idea is for this component to show a (paginated?) list of ALL CeR projects
-  // which will be admin-only
+  // TO DO: list should be paginated
 
-  constructor() { }
+  projects: any;
+  loading$ = new Subject<boolean>();
+  error: any;
 
-  ngOnInit(): void {
+  private querySubscription: Subscription;
+
+  constructor(
+    private apollo: Apollo,
+    private router: Router,
+  ) {}
+
+  async ngOnInit() {
+    this.loading$.next(true);
+    this.querySubscription = this.apollo
+    .watchQuery<any>({
+      query: GET_ALL_PROJECTS,
+    })
+    .valueChanges
+    .subscribe(
+      ({ data, loading }) => {
+        this.projects = data.projects;
+        this.loading$.next(loading);
+      },
+      error => {
+        this.loading$.next(false);
+        if (error.message === 'Not authorized!') {
+          this.router.navigate(['/error/403']);
+        }
+        if (error.message === 'GraphQL error: 404: NOT FOUND') {
+          this.projects = [];
+        }
+        else {
+          console.log(JSON.stringify(error));
+          this.error = error;
+        }
+      }
+    );
   }
 
+  openProject(projectId) {
+    this.router.navigateByUrl(`/project/${projectId}`);
+  }
+
+  ngOnDestroy(): void {
+    if (this.querySubscription) {
+      this.querySubscription.unsubscribe();
+    }
+  }
 }
