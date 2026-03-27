497f4078eb0cd8591a55f1127e4319a125af8eb8
rosemcc <rosemccoll@hotmail.com>
2026-03-27 15:22:57 +1300

    remove vm request component, link to hub page

--- src/app/modules/cer-services/cer-services.module.ts
+++ src/app/modules/cer-services/cer-services.module.ts
@@ -4,13 +4,11 @@ import { SharedModule } from '@shared/shared.module';
 import { CerServiceRoutingModule } from './cer-services.routing.module';
 import { CerServicesComponent } from './cer-services.component';
 import { RequestStorageComponent } from './request-storage/request-storage.component';
-import { RequestVmComponent } from './request-vm/request-vm.component';
 
 
 @NgModule({
   declarations: [
     RequestStorageComponent,
-    RequestVmComponent,
     CerServicesComponent
   ],
   imports: [
